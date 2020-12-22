7f3f37b2a83d29e177b39604152f58ec6e569b4d
Garrett Cox <garrettjcox@gmail.com>
2020-12-21 23:53:37 -0600

    Implement simple glow

--- src/base.service.ts
+++ src/base.service.ts
@@ -1,134 +1,136 @@
 import * as rxjs from 'rxjs';
-import {AfterEntityLoopData, BaseGameData, GameData, HackConfig} from './base.interfaces';
-import {ClientStateService} from './game/clientState/clientState.service';
-import {EntityBase} from './game/entity/entity.service';
-import {Player} from './game/player/player';
-import {RendererService} from './game/renderer/renderer.service';
-import {SocketService} from './network/socket/socket.service';
-import {offsets} from './offsets';
-import {ProcessService} from './process/process.service';
-import {Global} from './shared/declerations';
+import { AfterEntityLoopData, BaseGameData, GameData, HackConfig } from './base.interfaces';
+import { ClientStateService } from './game/clientState/clientState.service';
+import { EntityBase } from './game/entity/entity.service';
+import { Player } from './game/player/player';
+import { RendererService } from './game/renderer/renderer.service';
+import { SocketService } from './network/socket/socket.service';
+import { offsets } from './offsets';
+import { ProcessService } from './process/process.service';
+import { Global } from './shared/declerations';
 
 export let proc: ProcessService;
 
 export class BaseService {
-    private newDataSubject: rxjs.Subject<GameData> = new rxjs.Subject<GameData>();
+  private newDataSubject: rxjs.Subject<GameData> = new rxjs.Subject<GameData>();
 
-    private afterEntityLoopSubject: rxjs.Subject<AfterEntityLoopData> = new rxjs.Subject<AfterEntityLoopData>();
+  private afterEntityLoopSubject: rxjs.Subject<AfterEntityLoopData> = new rxjs.Subject<AfterEntityLoopData>();
 
-    private socketService: SocketService = new SocketService();
+  private socketService: SocketService = new SocketService();
 
-    private wsStarted: boolean;
+  private wsStarted: boolean;
 
-    constructor(public config: HackConfig = {
-        webSocketService: {
-            start: false,
-            socketServicePort: 8080,
-        },
-    }) {
-        console.log('base service init');
+  constructor(
+    public config: HackConfig = {
+      webSocketService: {
+        start: false,
+        socketServicePort: 8080,
+      },
     }
+  ) {
+    console.log('base service init');
+  }
 
-    run() {
-        if (this.wsShouldStart()) {
-            console.log('starting ws server..');
-            this.socketService.startServer(this.doRun.bind(this));
-            this.wsStarted = true;
-            return;
+  run() {
+    if (this.wsShouldStart()) {
+      console.log('starting ws server..');
+      this.socketService.startServer(this.doRun.bind(this));
+      this.wsStarted = true;
+      return;
+    }
+    this.doRun();
+  }
+
+  public onNewData(): rxjs.Subject<GameData> {
+    return this.newDataSubject;
+  }
+
+  public afterEntityLoop(): rxjs.Subject<AfterEntityLoopData> {
+    return this.afterEntityLoopSubject;
+  }
+
+  private wsShouldStart = () => (this.config.webSocketService ? this.config.webSocketService.start : false);
+
+  private getBaseReply = (): BaseGameData => ({
+    clientState: Global.clientState,
+    baseIsRunning: true,
+    entityBase: Global.entityBase,
+    localEntity: Global.entityBase.entity(Global.clientState.localEntityIndex),
+    player: Global.player,
+    sendMessageToEachWsClient: this.socketService.sendToEachClient.bind(this.socketService),
+    offsets: this.config.offsets,
+    getModuleBase: (moduleName: string) => Global.gM(moduleName).modBaseAddr,
+    readMemory: Global.rpm,
+  });
+
+  private declareGlobal(processService: ProcessService) {
+    Global.gM = processService.getModule.bind(proc);
+    Global.rpm = processService.readMemory.bind(proc);
+    Global.rbf = processService.readBuffer.bind(proc);
+    Global.wpm = processService.writeMemory.bind(proc);
+    Global.wbf = processService.writeBuffer.bind(proc);
+
+    Global.clientState = new ClientStateService(offsets);
+    Global.entityBase = new EntityBase(offsets);
+    Global.renderer = new RendererService(offsets);
+    Global.player = new Player(offsets);
+  }
+
+  private doRun() {
+    proc = new ProcessService('csgo.exe');
+    this.declareGlobal(proc);
+    console.log('hack initialized..\nstarting main loop..');
+
+    const update = () => {
+      Global.clientState.update();
+      Global.entityBase.update(Global.clientState.localEntityIndex);
+      Global.renderer.readViewMatrix();
+    };
+
+    update();
+    const main = rxjs.interval(0);
+
+    main.subscribe(() => {
+      const loaded = [];
+      for (let i = 0; i < Global.clientState.maxEntitys; i += 3) {
+        if (!loaded.includes(i)) {
+          loaded.push(i);
+          this.sendNewPlayerDataByIndex(i);
         }
-        this.doRun();
-    }
+        if (!loaded.includes(i + 1) && i + 1 < Global.clientState.maxEntitys) {
+          this.sendNewPlayerDataByIndex(i + 1);
+          loaded.push(i + 1);
+        }
+        if (!loaded.includes(i + 2) && i + 2 < Global.clientState.maxEntitys) {
+          this.sendNewPlayerDataByIndex(i + 2);
+          loaded.push(i + 2);
+        }
+      }
 
-    public onNewData(): rxjs.Subject<GameData> {
-        return this.newDataSubject;
-    }
-
-    public afterEntityLoop(): rxjs.Subject<AfterEntityLoopData> {
-        return this.afterEntityLoopSubject;
-    }
-
-    private wsShouldStart = () => (this.config.webSocketService ? this.config.webSocketService.start : false);
-
-    private getBaseReply = (): BaseGameData => ({
-        clientState: Global.clientState,
-        baseIsRunning: true,
-        entityBase: Global.entityBase,
+      update();
+      // on update
+      this.afterEntityLoopSubject.next({
+        ...this.getBaseReply(),
         localEntity: Global.entityBase.entity(Global.clientState.localEntityIndex),
         player: Global.player,
-        sendMessageToEachWsClient: this.socketService.sendToEachClient.bind(this.socketService),
-        offsets: this.config.offsets,
-        getModuleBase: (moduleName: string) => Global.gM(moduleName).modBaseAddr,
-        readMemory: Global.rpm,
+      });
     });
+  }
 
-    private declareGlobal(processService: ProcessService) {
-        Global.gM = processService.getModule.bind(proc);
-        Global.rpm = processService.readMemory.bind(proc);
-        Global.rbf = processService.readBuffer.bind(proc);
-        Global.wpm = processService.writeMemory.bind(proc);
-
-        Global.clientState = new ClientStateService(offsets);
-        Global.entityBase = new EntityBase(offsets);
-        Global.renderer = new RendererService(offsets);
-        Global.player = new Player(offsets);
-    }
-
-    private doRun() {
-        proc = new ProcessService('csgo.exe');
-        this.declareGlobal(proc);
-        console.log('hack initialized..\nstarting main loop..');
-
-        const update = () => {
-            Global.clientState.update();
-            Global.entityBase.update(Global.clientState.localEntityIndex);
-            Global.renderer.readViewMatrix();
-        };
-
-        update();
-        const main = rxjs.interval(0);
-
-        main.subscribe(() => {
-            let loaded = [];
-            for (let i = 0; i < Global.clientState.maxEntitys; i += 3) {
-                if (!loaded.includes(i)) {
-                    loaded.push(i);
-                    this.sendNewPlayerDataByIndex(i);
-                }
-                if (!loaded.includes(i + 1) && i + 1 < Global.clientState.maxEntitys) {
-                    this.sendNewPlayerDataByIndex(i + 1);
-                    loaded.push(i + 1);
-                }
-                if (!loaded.includes(i + 2) && i + 2 < Global.clientState.maxEntitys) {
-                    this.sendNewPlayerDataByIndex(i + 2);
-                    loaded.push(i + 2);
-                }
-
-            }
-
-            update();
-            // on update
-            this.afterEntityLoopSubject.next({
-                ...this.getBaseReply(),
-                localEntity: Global.entityBase.entity(Global.clientState.localEntityIndex),
-                player: Global.player,
-            });
+  private sendNewPlayerDataByIndex(i: number) {
+    Global.entityBase.update(i);
+    const entity = Global.entityBase.entity(i);
+    if (entity && entity.lifeState === 0) {
+      if (entity.team === 2 || entity.team === 3) {
+        this.newDataSubject.next({
+          ...this.getBaseReply(),
+          currentEntity: entity,
+          localEntity: Global.entityBase.entity(Global.clientState.localEntityIndex),
+          currentEntityIndex: i,
+          player: Global.player,
         });
+        // forEachPlayer(entity, i);
+      }
     }
-
-    private sendNewPlayerDataByIndex(i: number) {
-        Global.entityBase.update(i);
-        const entity = Global.entityBase.entity(i);
-        if (entity && entity.lifeState === 0) {
-            if ((entity.team === 2 || entity.team === 3)) {
-                this.newDataSubject.next({
-                    ...this.getBaseReply(),
-                    currentEntity: entity,
-                    localEntity: Global.entityBase.entity(Global.clientState.localEntityIndex),
-                    currentEntityIndex: i,
-                    player: Global.player,
-                });
-                // forEachPlayer(entity, i);
-            }
-        }
-    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,40 @@
-import {BaseService} from './base.service';
-import {ExtendedMath, Vec3} from './math/extendedMath.service';
-import {getOffsets} from './offsets';
-import {MemoryTypes} from './process/process.interfaces';
-import {Global} from './shared/declerations';
+import { filter } from 'rxjs/operators';
+import { BaseService } from './base.service';
+import { PlayerEntity } from './game/entity/entity.interfaces';
+import { getOffsets } from './offsets';
+import { MemoryTypes } from './process/process.interfaces';
+import { Global } from './shared/declerations';
 
-getOffsets().then((offsets) => {
+getOffsets()
+  .then((offsets) => {
     const base: BaseService = new BaseService({
-        webSocketService: {
-            start: false,
-            socketServicePort: 8080,
-        },
-        offsets,
+      webSocketService: {
+        start: false,
+        socketServicePort: 8080,
+      },
+      offsets,
     });
 
     base.run();
 
-    /*let allLocations = [];
-    let position: any;
-    let viewAngles: any;
-    let vecView: any;*/
-
-    base.afterEntityLoop().subscribe((data) => {
-        /*if (data.localEntity.crosshairEntity) {
-            // data.entityBase.update(data.localEntity.crosshairEntity.index);
-            const headPosLocal = ExtendedMath.addVec(
-                data.localEntity.origin, data.localEntity.vecView);
-            const headPosEnemy = data.localEntity.crosshairEntity.getBonePosition(8);
-            const aimAngles = ExtendedMath.calcAngle(headPosLocal, headPosEnemy);
-            data.clientState.viewAngles = ExtendedMath.subVec(
-                aimAngles, ExtendedMath.multiplyVec(data.localEntity.punchAngles, 2.0));
-            data.player.attack();
-        }*/
-        /*if (base.config.webSocketService.start) {
-            data.sendMessageToEachWsClient(
-                JSON.stringify({entities: allLocations, local: {position, viewAngles, vecView}}));
-        }
-        allLocations = [];*/
-    });
-
-    base.onNewData().subscribe((data) => {
-        const headPosLocal = ExtendedMath.addVec(
-            data.localEntity.origin, data.localEntity.vecView);
-        const headPosEnemy = data.currentEntity.getBonePosition(8);
-        const aimAngles = ExtendedMath.calcAngle(headPosLocal, headPosEnemy);
-        const angleDiff = ExtendedMath.subVec(data.clientState.viewAngles, aimAngles);
-        const fov = 10;
-        if (((angleDiff.x > 0 && angleDiff.x <= fov) || (angleDiff.x < 0 && angleDiff.x >= fov * -1)) &&
-            ((angleDiff.y > 0 && angleDiff.y <= fov) || (angleDiff.y < 0 && angleDiff.y >= fov * -1))) {
-            if (data.currentEntity.team !==  data.localEntity.team && data.currentEntity.health >= 1) {
-                data.clientState.viewAngles = ExtendedMath.subVec(
-                    aimAngles, ExtendedMath.multiplyVec(data.localEntity.punchAngles, 2.0));
-                data.player.attack();
-            }
-        } else {
-        }
-        /*const crosshairId = data.localEntity.read<number>(data.offsets.netvars.m_iCrosshairId, MemoryTypes.int);
-        allLocations.push(data.currentEntity.origin);
-        position = data.localEntity.origin;
-        viewAngles = data.clientState.viewAngles;
-        vecView = data.localEntity.vecView;*/
-
-        // console.log(data.localEntity.weaponEntity.name);
-        // console.log(data.localEntity.weaponEntity.id);
-        // There are Three ways to get data:
-
-        // 1. Reading a buffer and then getting data from this buffer.
-        //  const healthBuffer = data.localEntity.readBuffer(offsets.netvars.m_iHealth, 4);
-        //  const health = healthBuffer.readIntLE(0, 4);
-
-        // 2. Using quick access for the most basic entity vars.
-        //  const health = data.localEntity.health;
-
-        // 3. Reading a type by offset
-        //  const health = data.localEntity.read<number>(offsets.netvars.m_iHealth,MemoryTypes.int);
-        //  console.log(data.localEntity.health);
-
-        // To get the bone position of a PlayerEntity, you can do one of the following executions
-
-        // 1. To get the bone data for the currentLoop PlayerEntity
-        //  console.log(data.currentEntity.getBonePosition(8));
-        // or 2. To get the bone position for a entity by index ( index 10 in this case
-
-        //  console.log(data.entityBase.entity(10).getBonePosition(8));
-    });
-}).catch((e) => {
-    console.log('couldnt load offsets...');
-});
+    base
+      .onNewData()
+      .pipe(filter((d) => d.currentEntity.lifeState === 0 && d.currentEntity.team === 3))
+      .subscribe((data) => {
+        const entity: PlayerEntity = data.currentEntity;
+        const glowIndex: number = entity.read(offsets.netvars.m_iGlowIndex, MemoryTypes.uint32);
+        const glowManager: number = Global.rpm(
+          Global.gM('client.dll').modBaseAddr + offsets.signatures.dwGlowObjectManager,
+          MemoryTypes.uint32
+        );
+        Global.wpm(glowManager + glowIndex * 0x38 + 0x4, 0, MemoryTypes.float);
+        Global.wpm(glowManager + glowIndex * 0x38 + 0x8, 1, MemoryTypes.float);
+        Global.wpm(glowManager + glowIndex * 0x38 + 0xc, 0, MemoryTypes.float);
+        Global.wpm(glowManager + glowIndex * 0x38 + 0x10, 1, MemoryTypes.float);
+        Global.wpm(glowManager + glowIndex * 0x38 + 0x24, true, MemoryTypes.bool);
+        Global.wpm(glowManager + glowIndex * 0x38 + 0x25, false, MemoryTypes.bool);
+      });
+  })
+  .catch((e) => {
+    console.error(e);
+  });
