5ccb9644a85de50af5df0513ba42a7c8701b300a
Garrett Cox <garrettjcox@gmail.com>
2020-12-23 17:56:42 -0600

    Wipe source

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { Vec3, Vec2 } from './math/extendedMath.service';
-
-export { BaseService } from './base.service';
-export { getOffsets } from './offsets';
-export { MemoryTypes } from './process/process.interfaces';
+(() => {
+  console.log('test');
+})();
